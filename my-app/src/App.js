b7b9eb724064ee6224383a5b5aecbdad6757bc2e
annnguyenn <thuanann.nguyen@gmail.com>
2022-09-15 12:56:37 +0700

    tictactoe

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Game from './board';
+
+function App() {
+  return (
+    <div className="App">
+      <Game />
+    </div>
+  );
+}
+
+export default App;
